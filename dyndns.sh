7f71a681f8564a8aa7bb60019e697704f06a59db
Marcel Fliegel <marcel@Marcels-MBP.flikra.net>
2020-07-26 11:18:31 +0200

    [hetzner-api-dyndns-2007250936] include more case options

--- dyndns.sh
+++ dyndns.sh
@@ -1,66 +1,75 @@
 #!/bin/bash
-# DynDNS Script for Hetzner API by FarrowStrange
+# DynDNS Script for Hetzner DNS API by FarrowStrange
 # v1.0
 
+auth_api_token=''
 record_ttl='60'
 record_type='A'
 
+
 display_help() {
   cat <<EOF
-exec: ./dyndns.sh -a <Auth-API-Token> -z <Zone ID> -r <Record ID> -n <Record Name>
+
+exec: ./dyndns.sh -z <Zone ID> -r <Record ID> -n <Record Name>
 
 parameters:
-  -a  - Auth-API-Token
   -z  - Zone ID
   -r  - Record ID
   -n  - Record name
 
 optional parameters:
-  -t  - TTL
-  -T  - Type
+  -t  - TTL (Default: 60)
+  -T  - Record type (Default: A)
 
 help:
   -h  - Show Help 
 
 example:
-  .exec: ./dyndns.sh -a 234hj23S7d9asd213 -z 98jFjsd8dh1GH7 -r AHD82h347fGAF1 -n dyn
+  .exec: ./dyndns.sh -z 98jFjsd8dh1GH7 -r AHD82h347fGAF1 -n dyn
+
 EOF
   exit 1
 }
 
-while getopts ":a:z:r:n:tT" opt; do
+while getopts ":z:r:n:tTh" opt; do
   case "$opt" in
-    a )
-      auth_api_token="${OPTARG}"
-      ;;
-    z )
-      zone_id="${OPTARG}"
-      ;;
-    r )
-     record_id="${OPTARG}"
-      ;;
-    n )
-      record_name="${OPTARG}"
-      ;;
-    t )
-      record_ttl="${OPTARG}"
-      ;;
-    T )
-      record_type="${OPTARG}"
-      ;;
-    h )
-      display_help
-      ;;
+    z  ) zone_id="${OPTARG}";;
+    r  ) record_id="${OPTARG}";;
+    n  ) record_name="${OPTARG}";;
+    t  ) record_ttl="${OPTARG}";;
+    T  ) record_type="${OPTARG}";;
+    h  ) display_help;;
+    \? ) echo "Invalid option: -$OPTARG" >&2; exit 1;;
+    :  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+    *  ) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
   esac
 done
 
+if [[ "${zone_id}" = "" ]]; then 
+  echo "Missing option for zone ID: -z <Zone ID>"
+  echo "Use -h to display help."
+  exit 1
+elif [[ "${record_id}" = "" ]]; then
+  echo "Mission option for record ID: -r <Record ID>"
+  echo "Use -h to display help."
+  exit 1
+elif [[ "${record_name}" = "" ]]; then
+  echo "Mission option for record name: -n <Record Name>"
+  echo "Use -h to display help."
+  exit 1
+fi
 
+if [[ "${auth_api_token}" = "" ]]; then
+  echo "No Auth API Token specified. Please reference at the top of the Script."
+  exit 1
+fi
 
-pub_addr=`curl -s ifconfig.me`
-api_dyn_addr=`curl -s "https://dns.hetzner.com/api/v1/records/${record_id}" -H 'Auth-API-Token: '${auth_api_token} | cut -d ',' -f 4 | cut -d '"' -f 4`
+cur_pub_addr=`curl -s ifconfig.me`
+cur_dyn_addr=`curl -s "https://dns.hetzner.com/api/v1/records/${record_id}" -H 'Auth-API-Token: '${auth_api_token} | cut -d ',' -f 4 | cut -d '"' -f 4`
 
-if [[ $pub_addr == $api_dyn_addr ]]; then
+if [[ $cur_pub_addr == $cur_dyn_addr ]]; then
   echo "DNS record is up to date - nothing to to."
+  exit 0
 else
   echo "DNS record is no longer valid - updating record" 
 
@@ -68,7 +77,7 @@ else
     -H 'Content-Type: application/json' \
     -H 'Auth-API-Token: '${auth_api_token} \
     -d $'{
-      "value": "'${pub_addr}'",
+      "value": "'${cur_pub_addr}'",
       "ttl": '${record_ttl}',
       "type": "'${record_type}'",
       "name": "'${record_name}'",
