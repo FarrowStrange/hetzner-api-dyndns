cc3cfbde72673328aec76fc350abbbbebb8d5124
Marcel Fliegel <marcel@Marcels-MacBook-Pro.local>
2020-11-24 21:52:10 +0100

    [hetzner-api-dyndns-2011212204] check api_token and get public IP

--- python/dyndns.py
+++ python/dyndns.py
@@ -2,6 +2,11 @@
 # v1.0
 
 import argparse
+import json
+import urllib.request
+import requests
+
+auth_api_token = ""
 
 api_parser = argparse.ArgumentParser(description='DynDNS Python Script for Hetzner DNS API:')
 positional_group = api_parser.add_argument_group('positional arguments')
@@ -26,9 +31,12 @@ api_parser.add_argument('-T','--type',
                         metavar='',
                         default='A',
                         help='Record type (Default: A)')
-
 args = api_parser.parse_args()
 
-print(args.zone)
-print(args.record)
-print(args.name)
+if not auth_api_token:
+    print("No Auth API Token specified. Please reference at the top of the Script.")
+    exit(1)
+
+pub_addr_info = json.loads(urllib.request.urlopen('https://ifconfig.me/all.json').read().decode('utf-8'))
+cur_pub_addr = pub_addr_info['ip_addr']
+
