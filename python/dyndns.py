99d10f6d642a8628de2badb0f96fd6b8f3c139d6
Marcel Fliegel <marcel@Marcels-MBP.flikra.net>
2020-12-14 20:16:33 +0100

    [hetzner-api-dyndns-2011212204] added record functions

--- python/dyndns.py
+++ python/dyndns.py
@@ -1,12 +1,47 @@
 # DynDNS Script for Hetzner DNS API by FarrowStrange
 # v1.0
 
+api_token = ""
+
+
 import argparse
 import json
 import urllib.request
 import requests
 
-auth_api_token = ""
+
+def get_record():
+  try:
+    response = requests.get(
+      url=f"https://dns.hetzner.com/api/v1/records/{args.record}",
+      headers={"Auth-API-Token": f"{api_token}"},
+    )
+    print('{content}'.format(
+      content=response.content))
+  except requests.exceptions.RequestException:
+    print('HTTP Request failed')
+
+def update_record():
+  try:
+    response = requests.put(
+      url=f"https://dns.hetzner.com/api/v1/records/{args.record}",
+      headers={
+        "Content-Type": "application/json",
+        "Auth-API-Token": f"{api_token}",
+      },
+      data=json.dumps({
+        "value": f"{pub_addr}",
+        "ttl": 0,
+        "type": f"{args.type}",
+        "name": f"{args.name}",
+        "zone_id": f"{args.zone}"
+      })
+    )
+    print('Response HTTP Response Body: {content}'.format(
+      content=response.content))
+  except requests.exceptions.RequestException:
+      print('HTTP Request failed')
+
 
 api_parser = argparse.ArgumentParser(description='DynDNS Python Script for Hetzner DNS API:')
 positional_group = api_parser.add_argument_group('positional arguments')
@@ -33,10 +68,9 @@ api_parser.add_argument('-T','--type',
                         help='Record type (Default: A)')
 args = api_parser.parse_args()
 
-if not auth_api_token:
+if not api_token:
     print("No Auth API Token specified. Please reference at the top of the Script.")
     exit(1)
 
 pub_addr_info = json.loads(urllib.request.urlopen('https://ifconfig.me/all.json').read().decode('utf-8'))
-cur_pub_addr = pub_addr_info['ip_addr']
-
+pub_addr = pub_addr_info['ip_addr']
